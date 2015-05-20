f3042ec07d488d3e6ce199ba2ad88bd9718dd5b1
Dwayne Bailey <dwayne@translate.org.za>
2015-05-20 13:16:01 +0100

    l20n: handle merging of two l20n file including plural handling

--- translate/convert/l20n2po.py
+++ translate/convert/l20n2po.py
@@ -6,6 +6,7 @@ See: http://docs.translatehouse.org/projects/translate-toolkit/en/latest/command
 for examples and usage instructions.
 """
 
+from translate.lang import data
 from translate.storage import po, l20n
 
 class l20n2po:
@@ -19,8 +20,8 @@ class l20n2po:
         po_units = []
 
         po_unit = po.pounit(encoding="UTF-8")
-        po_unit.addlocation(unit.getid())
         po_unit.setid(unit.getid())
+        po_unit.addlocation(unit.getid())
         if unit.value_index:
             # the value here may be a string or a hash
             # if it's a hash it will look like this:
@@ -31,7 +32,9 @@ class l20n2po:
                 # While l20n could potentially have N forms we can only handle
                 # and only want two in Gettext. Since Gettext uses English
                 # forms we're using the same: 'one' and 'other'
+                po_unit.addnote("<l20n:plural>@cldr.plural($%s)</l20n>" % unit.value_index[1], "developer")
                 po_unit.source = [unit.value['one'], unit.value['other']]
+                po_unit.target = ["", ""]
         else:
             po_unit.source = unit.value
         po_units.append(po_unit)
@@ -66,14 +69,21 @@ class l20n2po:
         targetheader.addnote("extracted from %s, %s" % (origl20nfile.filename,
                                                         translatedl20nfile.filename),
                              "developer")
-        origl20nfile.makeindex()
+        translatedl20nfile.makeindex()
         for l20nunit in origl20nfile.units:
             pounits = self.convertl20nunit(origl20nfile, l20nunit)
             for pounit in pounits:
-                target_store.addunit(pounit)
-                if pounit.getid() in translatedl20nfile.locationindex:
-                    print('fooo')
-                    print(pounit.name)
+                newunit = target_store.addunit(pounit)
+                for location in pounit.getlocations():
+                    if location in translatedl20nfile.sourceindex:
+                        if isinstance(translatedl20nfile.sourceindex[location][0].value, dict):
+                            targets = []
+                            for form in data.cldr_plural_categories:
+                                if form in translatedl20nfile.sourceindex[location][0].value:
+                                    targets.append(translatedl20nfile.sourceindex[location][0].value[form])
+                            pounit.target = targets
+                        else:
+                            pounit.target = translatedl20nfile.sourceindex[location][0].value
         return target_store
         
 
